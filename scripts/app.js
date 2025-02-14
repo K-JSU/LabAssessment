69bb9838e4c47ca788f57e2e64c45f73738bce48
K-JSU <karthiksud13@gmail.com>
2025-02-13 19:32:10 -0500

    lab assessment

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const studentFullName="Karthik Jeeja Sudhakar";
+const studentNumber= 1200999;
+const result= `${studentFullName}-${studentNumber}`;
+console.log(result);
+const headerContent=document.querySelector(<h1></h1>);
+console.log(headerContent.textContent);
+
