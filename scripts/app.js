3739755e6d6ad13b1f5afcb9c4e117fc9fd10bd6
K-JSU <karthiksud13@gmail.com>
2025-02-13 19:40:26 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@ const studentFullName="Karthik Jeeja Sudhakar";
 const studentNumber= 1200999;
 const result= `${studentFullName}-${studentNumber}`;
 console.log(result);
-const headerContent=document.querySelector(<h1></h1>);
+const headerContent=document.querySelector("h1");
 console.log(headerContent.textContent);
+headerContent.textContent=result;
+headerContent.classList.add("headingPrimary")
 
