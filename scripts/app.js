793dcecdcbd0f653f818501e05b904cbd50daec6
K-JSU <karthiksud13@gmail.com>
2025-02-13 19:50:14 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,5 +5,5 @@ console.log(result);
 const headerContent=document.querySelector("h1");
 console.log(headerContent.textContent);
 headerContent.textContent=result;
-headerContent.classList.add("headingPrimary")
+headerContent.classList.add("headingPrimary");
 
